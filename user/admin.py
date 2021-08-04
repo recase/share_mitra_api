8ed760c8584d4f0ebe4a373185c251f42eafb368
Rikesh <recasechhunju@gmail.com>
2021-08-04 21:07:22 +0545

    Register user to the admin view

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'first_name',
+         'middle_name', 'last_name', )}),
+        ('permissions', {'fields': ('role', 'user_permissions', 'groups')})
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+        ('personnel', {'fields': ('first_name', 'middle_name', 'last_name')}),
+        ('permissions', {'fields': ('role', 'user_permissions', 'groups')})
+    )
+    list_display = ('id', 'email', 'first_name',
+                    'last_name', 'role', 'date_joined',)
+    ordering = ('id', 'email', 'first_name', 'last_name',)
+    list_filter = ('role',)
+    search_fields = ('email', 'first_name', 'last_name')
+
+
+admin.site.register(User, CustomUserAdmin)
