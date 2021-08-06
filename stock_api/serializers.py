8850c0db1bc702b3ba792942a1e11eb90492ed26
Rikesh <recasechhunju@gmail.com>
2021-08-06 18:16:56 +0545

    Add serilizers for the sector, company, price list models

---
+++ stock_api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Company, Sector, LivePirce, StockPrice
+
+
+class SectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sector
+        fields = ['id', 'name', 'regulatory_body']
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['id', 'name', 'symbol', 'sector']
+
+
+class LivePriceSerializer(serializers.ModelSerializer):
+    symbol = serializers.StringRelatedField(source='symbol', read_only=True)
+
+    class Meta:
+        model = LivePirce
+        fields = ['id', 'symbol', 'open_price', 'high_price', 'low_price', 'previous_close', 'last_traded_price',
+                  'total_volume', 'percentage_change', "last_updated_time"]
+
+
+class StockPriceSerializer(serializers.ModelSerializer):
+    symbol = serializers.StringRelatedField(source='symbol', read_only=True)
+
+    class Meta:
+        model = StockPrice
+        fields = ['id', 'symbol', 'open_price', 'high_price', 'low_price', 'previous_price',
+                  'close_price', 'percentage_change', 'total_volume', 'date']
