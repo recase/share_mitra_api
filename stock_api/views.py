cde36ff863cf8a651a1bc07b425fd1a88aebd04a
Rikesh <recasechhunju@gmail.com>
2021-08-06 18:20:39 +0545

    Add views for the sector and company model

--- stock_api/views.py
+++ stock_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from .models import Company, Sector
+from .serializers import CompanySerializer, SectorSerializer
 
-# Create your views here.
+
+class SectorViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    def list(self, request):
+        query_set = Sector.objects.all()
+        serializer = SectorSerializer(query_set, many=True)
+        return Response(serializer.data)
+
+
+class CompanyViewSet(viewsets.ViewSet):
+    def list(self, request):
+        query_set = Company.objects.all()
+        serializer = CompanySerializer(query_set, many=True)
+        return Response(serializer.data)
