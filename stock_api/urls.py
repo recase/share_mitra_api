a6c2b824b3e56f01ab22e7c5c6fa8b41b1f85d0b
Rikesh <recasechhunju@gmail.com>
2021-08-07 13:56:46 +0545

    Add endpoints for price list

--- stock_api/urls.py
+++ stock_api/urls.py
@@ -1,11 +1,12 @@
-from rest_framework import urlpatterns
 from rest_framework.routers import DefaultRouter
-from .views import CompanyViewSet, SectorViewSet
+from .views import LivePirceViewSet, SectorViewSet, CompanyViewSet, StockPriceViewSet
 
 app_name = "stock_api"
 
 router = DefaultRouter()
 router.register(r'sector', SectorViewSet, basename="sector")
 router.register(r'company', CompanyViewSet, basename="company")
+router.register(r'live-pirce', LivePirceViewSet, basename="live-price")
+router.register(r'stock-price', StockPriceViewSet, basename="stock-price")
 
 urlpatterns = router.urls
