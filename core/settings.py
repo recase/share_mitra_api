4a5871b8578de5a34eed972100c5e083c84d92d5
Rikesh <recasechhunju@gmail.com>
2021-08-07 13:55:27 +0545

    Add API documentaition with coreapi

--- core/settings.py
+++ core/settings.py
@@ -148,7 +148,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 AUTH_USER_MODEL = 'user.User'
--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls', namespace="user")),
     path('stock-api/', include('stock_api.urls', namespace="stock_api")),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('doc', include_docs_urls(title="shareMitraAPI"))
 ]
