14f95d969f6776c9081945d505f37bf048923aa2
Rikesh <recasechhunju@gmail.com>
2021-08-14 22:03:04 +0545

    Used custom manager

--- stock_api/helper/stock_price_list.py
+++ stock_api/helper/stock_price_list.py
@@ -2,8 +2,8 @@ from ..models import StockPrice
 
 
 def retrieve_stock_price_list(date):
-    return StockPrice.objects.filter(date=date)
+    return StockPrice.custom_objects.filter(date=date)
 
 
 def retrieve_stock_price_history(company_id):
-    return StockPrice.objects.filter(company_id=company_id).order_by('date')
+    return StockPrice.custom_objects.filter(company_id=company_id).order_by('date')
