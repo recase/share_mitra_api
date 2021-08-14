a2a2463731034997c2572a4269c2fac625c5a377
Rikesh <recasechhunju@gmail.com>
2021-08-14 22:01:21 +0545

    Update models and serializer

--- stock_api/models.py
+++ stock_api/models.py
@@ -10,11 +10,19 @@ class Sector(models.Model):
 
 
 class Company(models.Model):
+
+    class Companyobject(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().order_by('symbol')
+
     name = models.CharField(max_length=200)
     symbol = models.CharField(max_length=20, unique=True)
-    instrument_type = models.CharField(max_length=20)
+    instrument_type = models.CharField(max_length=100)
     sector = models.ForeignKey(
         Sector, related_name='companies', on_delete=models.CASCADE)
+
+    objects = models.Manager()
+    company_objects = Companyobject()
 
     def __str__(self):
         return self.name
@@ -25,7 +33,12 @@ class Company(models.Model):
 
 class LivePirce(models.Model):
     # live market data
-    company_id = models.OneToOneField(
+
+    class CustomLivePriceManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().order_by('company__symbol')
+
+    company = models.OneToOneField(
         Company, primary_key=True, on_delete=models.CASCADE)
     open_price = models.FloatField()
     high_price = models.FloatField()
@@ -35,6 +48,10 @@ class LivePirce(models.Model):
     total_volume = models.IntegerField()
     percentage_change = models.FloatField()
     last_updated_time = models.DateTimeField()
+    last_traded_volume = models.FloatField()
+
+    objects = models.Manager()
+    custom_objects = CustomLivePriceManager()
 
     def save(self, *args, **kwargs):
         self.percentage_change = round(self.percentage_change, 3)
@@ -43,19 +60,33 @@ class LivePirce(models.Model):
 
 class StockPrice(models.Model):
     # historical stock data
-    company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    class customStockPriceObject(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().order_by('company__symbol')
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     open_price = models.FloatField()
     high_price = models.FloatField()
     low_price = models.FloatField()
-    previous_price = models.FloatField()
+    previous_close_price = models.FloatField()
     close_price = models.FloatField()
     percentage_change = models.FloatField()
     total_volume = models.IntegerField()
     date = models.DateField()
 
+    objects = models.Manager()
+    custom_objects = customStockPriceObject()
+
     def save(self, *args, **kwargs):
         self.percentage_change = round(self.percentage_change, 3)
         super(StockPrice, self).save(*args, **kwargs)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['company', 'date'], name='stock traded day')
+        ]
 
 
 class StockUpdateTable(models.Model):
--- stock_api/serializers.py
+++ stock_api/serializers.py
@@ -9,26 +9,40 @@ class SectorSerializer(serializers.ModelSerializer):
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    sector = serializers.StringRelatedField(
+        source='sector.name', read_only=True)
+
     class Meta:
         model = Company
         fields = ['id', 'name', 'symbol', 'sector']
 
 
 class LivePriceSerializer(serializers.ModelSerializer):
-    symbol = serializers.StringRelatedField(source='symbol', read_only=True)
+    symbol = serializers.StringRelatedField(
+        source='company.symbol', read_only=True)
+    last_traded_time = serializers.CharField(source="last_updated_time")
+    change = serializers.CharField(source="percentage_change")
+    previous_close_price = serializers.CharField(source="previous_close")
 
     class Meta:
         model = LivePirce
-        fields = ['id', 'symbol', 'open_price', 'high_price', 'low_price', 'previous_close', 'last_traded_price',
-                  'total_volume', 'percentage_change', "last_updated_time"]
+        fields = ['symbol', 'open_price', 'high_price', 'low_price', 'previous_close_price', 'last_traded_price',
+                  'total_volume', 'change', "last_traded_time", "last_traded_volume"]
+
+
+class RoundingDecimalField(serializers.DecimalField):
+    def validate_precision(self, value):
+        return value
 
 
 class StockPriceSerializer(serializers.ModelSerializer):
-    symbol = serializers.StringRelatedField(source='symbol', read_only=True)
+    symbol = serializers.StringRelatedField(
+        source='company.symbol', read_only=True)
+    percentage_change = RoundingDecimalField(max_digits=4, decimal_places=2)
 
     class Meta:
         model = StockPrice
-        fields = ['id', 'symbol', 'open_price', 'high_price', 'low_price', 'previous_price',
+        fields = ['id', 'symbol', 'open_price', 'high_price', 'low_price', 'previous_close_price',
                   'close_price', 'percentage_change', 'total_volume', 'date']
 
 
@@ -38,3 +52,12 @@ class CompanyWithPriceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = ['id', 'name', 'sector', 'price_list']
+
+
+# change the fiekds name to different names
+# class ParkSerializer(serializers.ModelSerializer):
+    # location = serializers.CharField(source='otherModel__other_fields')
+
+    # class Meta:
+    #     model = Park
+    #     fields = ('other_fields', 'location')
