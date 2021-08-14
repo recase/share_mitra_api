6d9ef09848d8fe48910d5b6be377d984306fb130
Rikesh <recasechhunju@gmail.com>
2021-08-14 22:00:34 +0545

    update API end point

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls', namespace="user")),
-    path('stock-api/', include('stock_api.urls', namespace="stock_api")),
+    path('api/', include('stock_api.urls', namespace="stock_api")),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('doc', include_docs_urls(title="shareMitraAPI"))
 ]
