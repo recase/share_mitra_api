e370b1bea9e7bf3321084b6f9af93956d4e7d317
Rikesh <recasechhunju@gmail.com>
2021-08-17 14:19:12 +0545

    Register all models to admin view

--- stock_api/admin.py
+++ stock_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Sector, Company, StockPrice, StockUpdateTable, LivePirce
 
 # Register your models here.
+admin.site.register(Sector)
+admin.site.register(Company)
+admin.site.register(StockPrice)
+admin.site.register(StockUpdateTable)
+admin.site.register(LivePirce)
