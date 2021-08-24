b3692446141a1b2d476ec510195183372f3b4dbe
Rikesh <recasechhunju@gmail.com>
2021-08-24 12:08:57 +0545

    Add urls for user activation and static files

--- core/urls.py
+++ core/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from user.views import UserActivateInvalidTemplateView, UserActivateSuccessTemplateView, activate_email
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +26,16 @@ urlpatterns = [
     path('api/', include('stock_api.urls', namespace="stock_api")),
     path('api/', include('portfolio.urls', namespace="portfolio_api")),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('activate-email/<uidb64>/<token>',
+         activate_email, name="activate_email"),
+    path('user-activated', UserActivateSuccessTemplateView.as_view(),
+         name="user_activated"),
+    path('activate-invalid', UserActivateInvalidTemplateView.as_view(),
+         name="invalid_activation"),
     path('doc', include_docs_urls(title="shareMitraAPI"))
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
