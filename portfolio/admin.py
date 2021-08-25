dc9995b77d1995628de154f4aa749c310cd4b9f6
Rikesh <recasechhunju@gmail.com>
2021-08-25 20:01:03 +0545

    register models on admin view

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Transaction, Portfolio
+from .models import Transaction, Portfolio, Alert, WatchList, TargetStopLoss
 
 # Register your models here.
 
 admin.site.register(Portfolio)
 admin.site.register(Transaction)
+admin.site.register(Alert)
+admin.site.register(WatchList)
+admin.site.register(TargetStopLoss)
